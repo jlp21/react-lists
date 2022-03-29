4d6726d1d15829e4438b89f2b03be659ae7ab247
jlp21 <portelli21@gmail.com>
2022-03-28 21:06:20 -0400

    adding lists

---
+++ src/components/StudentList.js
@@ -0,0 +1,16 @@
+function StudentList() {
+    const students = ['Marc', 'Lucy', 'Anna'];
+   
+    return (
+      <div className="list">
+        <h2>Student List</h2>
+        
+        { students.map(name => {
+          return <p> { name } </p>
+        })}
+        
+      </div>
+    );
+  }
+   
+  export default StudentList;
