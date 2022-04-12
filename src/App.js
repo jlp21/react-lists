7fc43256a64f893e6f14b36e7b38a87641c9c4f2
jlp21 <portelli21@gmail.com>
2022-04-11 21:28:23 -0400

    adding movies

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import './App.css';
 import SimpleList from './components/SimpleList';
 import StudentList from './components/StudentList';
  
+// function App() {
+//   return (
+//     <div className="App">
+//       <SimpleList />
+
+//       <StudentList />
+
+//     </div>
+//   );
+// }
+ 
+// export default App;
+
+import MovieList from './components/MovieList';
+ 
 function App() {
   return (
     <div className="App">
-      <SimpleList />
-
-      <StudentList />
-      
+      {/* <List /> */}
+      {/* <StudentList /> */}
+      {/* <ProjectList /> */}
+ 
+      <MovieList />
     </div>
   );
 }
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,16 +3,38 @@ import { useState } from 'react';
 // import the array of movie objects
 import moviesData from '../movies-data.json';
  
+// function MovieList() {
+//   // Declare a state variable "movies"
+//   // and set the array from movies-data.json as the initial state
+//   const [movies, setMovies] = useState(moviesData);
+ 
+//   return (
+//     <div>
+//       <h2>Movie List</h2>
+//     </div>
+//   );
+// }
+ 
+// export default MovieList;
+
 function MovieList() {
-  // Declare a state variable "movies"
-  // and set the array from movies-data.json as the initial state
-  const [movies, setMovies] = useState(moviesData);
- 
-  return (
-    <div>
-      <h2>Movie List</h2>
-    </div>
-  );
-}
- 
-export default MovieList;
+    const [movies, setMovies] = useState(moviesData);
+   
+    return (
+      <div>
+        <h2>Movie List</h2>
+        {movies.map(movie => {
+          return (
+            <div key={movie._id} className="MovieCard">
+              <h3>{movie.title}</h3>
+              <p>Director: {movie.director}</p>
+              <p>Rating: {movie.IMDBRating}</p>
+              <button className="btn-delete">Delete </button>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+   
+  export default MovieList;
